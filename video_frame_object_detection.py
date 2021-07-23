edfd2ac1793de7b21f39626024c0e3ad859c75f0
Chirag Jain <chiragj1812@gmail.com>
2021-07-23 19:07:34 +0530

    object detection performed on live video stream with output printed on pyplot

--- video_frame_object_detection.py
+++ video_frame_object_detection.py
@@ -1,9 +1,10 @@
 # load yolov3 model and perform object detection
 import numpy as np
+import cv2 
 from numpy import expand_dims
-from keras.models import load_model
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
  
@@ -126,6 +127,19 @@ def load_image_pixels(filename, shape):
 	# add a dimension so that we have one sample
 	image = expand_dims(image, 0)
 	return image, width, height
+
+def VideoStreamModifyFrames(frame,shape):
+	width, height, _ = frame.shape
+	# load the image with the required size
+	image = cv2.resize(frame, shape)
+
+	# scale pixel values to [0, 1]
+	image = image.astype('float32')
+	image /= 255.0
+	# add a dimension so that we have one sample
+	image = expand_dims(image, 0)
+	return image, width, height
+
  
 # get all of the results above a threshold
 def get_boxes(boxes, labels, thresh):
@@ -166,46 +180,142 @@ def draw_boxes(filename, v_boxes, v_labels, v_scores):
 		pyplot.text(x1, y1, label, color='white')
 	# show the plot
 	pyplot.show()
+
+def draw_boxes_videoFrame(frame, v_boxes, v_labels, v_scores):
+	data = frame
+	# plot the image
+	pyplot.imshow(data)
+	# get the context for drawing boxes
+	ax = pyplot.gca()
+	# plot each box
+	for i in range(len(v_boxes)):
+		box = v_boxes[i]
+		# get coordinates
+		y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
+		# calculate width and height of the box
+		width, height = x2 - x1, y2 - y1
+		# create the shape
+		rect = Rectangle((x1, y1), width, height, fill=False, color='white')
+		# draw the box
+		ax.add_patch(rect)
+		# draw text and score in top left corner
+		label = "%s (%.3f)" % (v_labels[i], v_scores[i])
+		pyplot.text(x1, y1, label, color='white')
+	# show the plot
+	pyplot.show()
  
-# load yolov3 model
-model = load_model('model.h5')
-# define the expected input shape for the model
-input_w, input_h = 416, 416
-# define our new photo
-photo_filename = 'zebra.jpg'
-# load and prepare image
-image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
-# make prediction
-yhat = model.predict(image)
-# summarize the shape of the list of arrays
-print([a.shape for a in yhat])
-# define the anchors
-anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
-# define the probability threshold for detected objects
-class_threshold = 0.6
-boxes = list()
-for i in range(len(yhat)):
-	# decode the output of the network
-	boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
-# correct the sizes of the bounding boxes for the shape of the image
-correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
-# suppress non-maximal boxes
-do_nms(boxes, 0.5)
-# define the labels
-labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
-	"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
-	"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
-	"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
-	"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
-	"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
-	"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-	"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
-	"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
-	"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
-# get the details of the detected objects
-v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
-# summarize what we found
-for i in range(len(v_boxes)):
-	print(v_labels[i], v_scores[i])
-# draw what we found
-draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+
+def readLiveVideoStream():
+	# load yolov3 model
+	model = load_model('model.h5')
+	# define the expected input shape for the model
+	input_w, input_h = 416, 416
+
+	cap=cv2.VideoCapture(0)
+	while True:
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		# Our operations on the frame come here
+		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		
+		image, image_w, image_h = VideoStreamModifyFrames(frame, (input_w, input_h))
+		# make prediction
+		yhat = model.predict(image)
+		# define the anchors
+		anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+		# define the probability threshold for detected objects
+		class_threshold = 0.6
+		boxes = list()
+
+		for i in range(len(yhat)):
+			# decode the output of the network
+			boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
+		# correct the sizes of the bounding boxes for the shape of the image
+		correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
+		# suppress non-maximal boxes
+		do_nms(boxes, 0.5)
+		# define the labels
+		labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+			"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+			"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+			"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+			"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+			"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+			"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+			"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+			"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+			"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+		# get the details of the detected objects
+		v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+		# summarize what we found
+		for i in range(len(v_boxes)):
+			print(v_labels[i], v_scores[i])
+		
+		# Display the resulting frame
+		# draw what we found
+		draw_boxes_videoFrame(frame, v_boxes, v_labels, v_scores)
+
+		cv2.imshow('frame',frame)
+		# cv2.imshow('frame',gray)
+		
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+
+def workOnFrame(frame):
+	return
+
+def default():
+	# load yolov3 model
+	model = load_model('model.h5')
+	# define the expected input shape for the model
+	input_w, input_h = 416, 416
+	# define our new photo
+	photo_filename = 'zebra.jpg'
+	# load and prepare image
+	image, image_w, image_h = load_image_pixels(photo_filename, (input_w, input_h))
+	# make prediction
+	yhat = model.predict(image)
+	# summarize the shape of the list of arrays
+	print([a.shape for a in yhat])
+	# define the anchors
+	anchors = [[116,90, 156,198, 373,326], [30,61, 62,45, 59,119], [10,13, 16,30, 33,23]]
+	# define the probability threshold for detected objects
+	class_threshold = 0.6
+	boxes = list()
+	for i in range(len(yhat)):
+		# decode the output of the network
+		boxes += decode_netout(yhat[i][0], anchors[i], class_threshold, input_h, input_w)
+	# correct the sizes of the bounding boxes for the shape of the image
+	correct_yolo_boxes(boxes, image_h, image_w, input_h, input_w)
+	# suppress non-maximal boxes
+	do_nms(boxes, 0.5)
+	# define the labels
+	labels = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck",
+		"boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench",
+		"bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+		"backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+		"sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+		"tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana",
+		"apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+		"chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse",
+		"remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+		"book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+	# get the details of the detected objects
+	v_boxes, v_labels, v_scores = get_boxes(boxes, labels, class_threshold)
+	# summarize what we found
+	for i in range(len(v_boxes)):
+		print(v_labels[i], v_scores[i])
+	# draw what we found
+	draw_boxes(photo_filename, v_boxes, v_labels, v_scores)
+
+def main():
+	readLiveVideoStream()
+	# default()
+	return 0
+
+if __name__=="__main__":
+	main()
